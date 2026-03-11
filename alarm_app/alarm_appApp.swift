6ea12d3bba396573482520d96258111833e2dd45
malcolmkuo <malcolmkuo@gmail.com>
2026-03-11 12:36:49 -0400

    Initial Commit

---
+++ alarm_app/alarm_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  alarm_appApp.swift
+//  alarm_app
+//
+//  Created by Malcolm Kuo on 2026/3/11.
+//
+
+import SwiftUI
+
+@main
+struct alarm_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
